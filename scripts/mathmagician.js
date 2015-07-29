3f3b41a056dd102bbf6308fa3beba78ff2f3c55d
Wieschie <supernova190@gmail.com>
2015-07-29 10:55:52 -0500

    Created a scripts directory and added jquery + nicolas hoening's popup script for tooltips

--- scripts/mathmagician.js
+++ scripts/mathmagician.js
@@ -3,6 +3,8 @@ bank = document.getElementById('bank'),
 morg = document.getElementById('morg'),
 gold = document.getElementById('gold'),
 solo = document.getElementById('solo');
+click = document.getElementById('click');
+jugg = document.getElementById('jugg');
 
 siya.onkeyup = mathmagic
 
@@ -17,12 +19,19 @@ function mathmagic() {
 	
 		result = Math.ceil(fsiya * 0.93)
 		gold.value = !isNaN(result) ? result : '';
+		
 		if(fsiya<=693)
 			result=Math.ceil(fsiya*.9);
 		else
 			// https://www.reddit.com/r/ClickerHeroes/comments/3823wt/mathematical_analysis_of_lategame_for_most_idle/
 			result=Math.ceil(1.15*Math.pow(Math.log(3.25*Math.pow(fsiya,2)),.4)*Math.pow(fsiya,.8));
 		solo.value = !isNaN(result) ? result : '';
+		
+		result = Math.ceil(fsiya * 0.5)
+		click.value = !isNaN(result) ? result : '';
+		
+		result = Math.ceil(fsiya * 0.1)
+		jugg.value = !isNaN(result) ? result : '';
 };
 
 function level_siya(add_levels) {
